a4fce5d6cc9a3c40ec3d94a1f74c6273671dc06d
Sukhdeep singh <sukhdeepsingh141414@gmail.com>
2024-03-02 23:06:05 +0530

    changes

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Styled, { styled } from 'styled-components'
+
+function Header() {
+  return (
+    <Nav>
+        <Logo src="" />
+    </Nav>
+  )
+}
+
+export default Header
+
+
+const Nav = Styled.div`
+    height: 70px;
+    background: #090b13;
+
+`
+
+const Logo = styled.div`
+    
+`
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
