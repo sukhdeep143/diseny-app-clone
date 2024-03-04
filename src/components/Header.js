49114859b14cc66a68000693436c8815af2f1490
Sukhdeep singh <sukhdeepsingh141414@gmail.com>
2024-03-04 19:21:13 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import Styled, { styled } from 'styled-components'
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import Styled, { styled } from 'styled-components'
 import ImgSlider from './ImgSlider'
